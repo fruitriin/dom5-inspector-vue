ff0fa80f404e608c8410d5ab999a1b8e927660ee
Riin <nassii74@gmail.com>
2018-08-05 23:49:12 +0900

    init commit

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  lintOnSave: false,
+  baseUrl: '/htmlinsert',
+  outputDir: undefined,
+  assetsDir: 'assets/',
+  runtimeCompiler: undefined,
+  productionSourceMap: false,
+  parallel: undefined,
+  css: undefined
+};
